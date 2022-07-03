adbffd0a021af39da9788fb9aa9a6dfaeda99161
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-07-03 19:09:05 +0530

    Update battery_checker.h

--- battery_checker.h
+++ battery_checker.h
@@ -4,11 +4,13 @@ enum batteryStatus
 {
   TEMPERATURE_RANGE = 0,
   SOC_RANGE,
-  CHARGE_RATE
-}batteryStatus_e;
+  CHARGE_RATE,
+  MAX_PARAMETERS
+}bms_e;
 
 typedef struct
 {
+  int status;
   float min;
   float max;
-}batteryStatus_s;
+}bms_s;

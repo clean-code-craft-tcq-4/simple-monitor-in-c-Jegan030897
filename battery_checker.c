6f3ae3b6df53b339d46bec6a1a1e6ddedbf36601
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-07-03 19:39:47 +0530

    Update battery_checker.c

--- battery_checker.c
+++ battery_checker.c
@@ -6,39 +6,25 @@ bms_s battery_checkcase[MAX_PARAMETERS];
 
 void print_batteryStatus(void){
     if((TRUE == battery_checkcase[TEMPERATURE_RANGE].status) && (TRUE == battery_checkcase[SOC_RANGE].status) &&
-      (TRUE == battery_checkcase[SOC_RANGE].status))
-    {
+      (TRUE == battery_checkcase[SOC_RANGE].status)){ 
         printf("Battery Status: Good\n");
-    }
-    else
-    {
+    }else{ 
         printf("Battery Status: NOT OK\n");
     }
 }
 
 void updateBatteryStatus_ParameterRange(void){
-  battery_checkcase[TEMPERATURE_RANGE].status = FALSE;
-  battery_checkcase[TEMPERATURE_RANGE].min = 0;
-  battery_checkcase[TEMPERATURE_RANGE].max = 45;
-    
-  battery_checkcase[SOC_RANGE].status = FALSE;
-  battery_checkcase[SOC_RANGE].min = 20;
-  battery_checkcase[SOC_RANGE].max = 80;
-        
-  battery_checkcase[CHARGE_RATE].status = FALSE;
-  battery_checkcase[CHARGE_RATE].min = 1;
-  battery_checkcase[CHARGE_RATE].max = 0.8;    
+  battery_checkcase[TEMPERATURE_RANGE].bms_Parameter = {FALSE, 0, 45};
+  battery_checkcase[SOC_RANGE].bms_Parameter = {FALSE, 20, 80};
+  battery_checkcase[CHARGE_RATE].bms_Parameter = {FALSE, 1, 0.8};  
 }
 
 int battery_status(bms_e parametersToBe_checked, float checkValue)
 {
     if((checkValue < battery_checkcase[parametersToBe_checked].min) &&
-      (checkValue > battery_checkcase[parametersToBe_checked].max))
-    {
+      (checkValue > battery_checkcase[parametersToBe_checked].max)) {
         return battery_checkcase[parametersToBe_checked].status = FALSE;
-    }
-    else
-    {
+    }else{
         return battery_checkcase[parametersToBe_checked].status = TRUE;
     }
 }

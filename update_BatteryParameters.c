6f6d006530adc9c194ddc8318e078ac8073d1ab6
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-07-03 19:57:14 +0530

    Update update_BatteryParameters.c

--- update_BatteryParameters.c
+++ update_BatteryParameters.c
@@ -7,7 +7,7 @@ void updateBatteryStatus_ParameterRange(void){
   battery_checkcase[TEMPERATURE_RANGE].bms_Parameter.max = 45;
   
   battery_checkcase[SOC_RANGE].bms_Parameter = FALSE;
-  battery_checkcase[SOC_RANGE].min = 20
+  battery_checkcase[SOC_RANGE].min = 20;
   battery_checkcase[SOC_RANGE].max = 80;
   
   battery_checkcase[CHARGE_RATE].bms_Parameter = FALSE;  

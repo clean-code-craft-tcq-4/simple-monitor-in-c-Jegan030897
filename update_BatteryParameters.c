d03fe667308aee6060bc6b48626819b5f5786e14
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-07-03 19:54:06 +0530

    Create update_BatteryParameters.c

---
+++ update_BatteryParameters.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "battery_checker.h"
+
+void updateBatteryStatus_ParameterRange(void){
+  battery_checkcase[TEMPERATURE_RANGE].bms_Parameter.status = {FALSE, 0, 45};
+  battery_checkcase[TEMPERATURE_RANGE].bms_Parameter.min = 0;
+  battery_checkcase[TEMPERATURE_RANGE].bms_Parameter.max = 45;
+  
+  battery_checkcase[SOC_RANGE].bms_Parameter = FALSE;
+  battery_checkcase[SOC_RANGE].min = 20
+  battery_checkcase[SOC_RANGE].max = 80;
+  
+  battery_checkcase[CHARGE_RATE].bms_Parameter = FALSE;  
+  battery_checkcase[CHARGE_RATE].min = 0.8;  
+  battery_checkcase[CHARGE_RATE].max = 1;
+}

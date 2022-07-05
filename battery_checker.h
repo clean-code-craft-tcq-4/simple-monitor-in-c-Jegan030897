03eb8518bb50dc2a057b062b015cb93ec5ea539f
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-07-05 16:20:27 +0530

    Update battery_checker.h

--- battery_checker.h
+++ battery_checker.h
@@ -26,4 +26,3 @@ typedef struct
 extern bms_s battery_checkcase[MAX_PARAMETERS];
 
 void updateBatteryStatus_ParameterRange(bms_s *Battery_checkcase); 
-void print_BatteryStatus(bms_s *battery_Status);

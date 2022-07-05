a5449fb65aacad920648cfd5f5f746e05af5a811
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-07-05 15:50:46 +0530

    Update update_BatteryParameters.c

--- update_BatteryParameters.c
+++ update_BatteryParameters.c
@@ -2,15 +2,15 @@
 #include "battery_checker.h"
 
 void updateBatteryStatus_ParameterRange(bms_s *Battery_checkcase){
-  Battery_checkcase->bms_Parameter->status = FALSE;
+  Battery_checkcase->bms_Parameter.status = FALSE;
   Battery_checkcase->bms_Parameter.min = FALSE;
   Battery_checkcase->bms_Parameter.max = FALSE;
   
-  *(Battery_checkcase + 1)->bms_Parameter.status = FALSE;
-  *(Battery_checkcase + 1)->bms_Parameter.min = FALSE;
-  *(Battery_checkcase + 1)->bms_Parameter.max = FALSE;
+  (Battery_checkcase + 1)->bms_Parameter.status = FALSE;
+  (Battery_checkcase + 1)->bms_Parameter.min = FALSE;
+  (Battery_checkcase + 1)->bms_Parameter.max = FALSE;
   
-  *(Battery_checkcase + 2)->bms_Parameter.status = FALSE;
-  *(Battery_checkcase + 2)->bms_Parameter.min = FALSE;
-  *(Battery_checkcase + 2)->bms_Parameter.max = FALSE;
+  (Battery_checkcase + 2)->bms_Parameter.status = FALSE;
+  (Battery_checkcase + 2)->bms_Parameter.min = FALSE;
+  (Battery_checkcase + 2)->bms_Parameter.max = FALSE;
 }

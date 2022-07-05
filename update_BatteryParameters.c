78c71df585561e231a1d83d9389949eb8e2b4d07
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-07-05 15:35:39 +0530

    Update update_BatteryParameters.c

--- update_BatteryParameters.c
+++ update_BatteryParameters.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include "battery_checker.h"
 
-void updateBatteryStatus_ParameterRange(void){
-  battery_checkcase[TEMPERATURE_RANGE].bms_Parameter.status = {FALSE, 0, 45};
+void updateBatteryStatus_ParameterRange(bms_s *Battery_checkcase){
+  *(Battery_checkcase)->bms_Parameter.status = FALSE;
+  *(Battery_checkcase)->bms_Parameter.min = FALSE;
+  *(Battery_checkcase)->bms_Parameter.max = FALSE;
+  
+  *(Battery_checkcase + 1)->bms_Parameter.status = FALSE;
+  *(Battery_checkcase + 1)->bms_Parameter.min = FALSE;
+  *(Battery_checkcase + 1)->bms_Parameter.max = FALSE;
+  
+  *(Battery_checkcase + 2)->bms_Parameter.status = FALSE;
+  *(Battery_checkcase + 2)->bms_Parameter.min = FALSE;
+  *(Battery_checkcase + 2)->bms_Parameter.max = FALSE;
+  /*battery_checkcase[TEMPERATURE_RANGE].bms_Parameter.status = {FALSE, 0, 45};
   battery_checkcase[TEMPERATURE_RANGE].bms_Parameter.min = 0;
   battery_checkcase[TEMPERATURE_RANGE].bms_Parameter.max = 45;
   
@@ -12,5 +23,5 @@ void updateBatteryStatus_ParameterRange(void){
   
   battery_checkcase[CHARGE_RATE].bms_Parameter = FALSE;  
   battery_checkcase[CHARGE_RATE].min = 0.8;  
-  battery_checkcase[CHARGE_RATE].max = 1;
+  battery_checkcase[CHARGE_RATE].max = 1;*/
 }

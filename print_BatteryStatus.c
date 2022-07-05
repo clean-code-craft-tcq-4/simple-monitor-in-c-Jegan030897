a312bdc4901d73dc6d88f39533ec2f9cd343bea9
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-07-05 16:19:14 +0530

    Update print_BatteryStatus.c

--- print_BatteryStatus.c
+++ print_BatteryStatus.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "battery_checker.h"
 
 void print_BatteryStatus(bms_s *battery_Status){
   if(Battery_checkcase->bms_Parameter.status == TRUE && (Battery_checkcase+1)->bms_Parameter.status == TRUE
